f28d9062da40ee014dce5141560d320dd6ee96f6
BahtiyarDev <deadpoolbahti10@gmail.com>
2025-08-14 00:09:45 +0500

    home-work

--- HW8.py
+++ HW8.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #сделат свою прогамму контактов
     # должн быть поля name email number
     # Бонус редактирование имеющихся контактов
@@ -18,25 +17,4 @@ def read_contacts():
         return file.read()
     
 add_contact()
-=======
-#сделат свою прогамму контактов
-    # должн быть поля name email number
-    # Бонус редактирование имеющихся контактов
-
-# + доделать 26 номер
-
-
-def add_contact():
-    with open('Contacts.txt', 'a', encoding='utf8') as file:
-        file.write(f"Имя: {input("Введите имя конттакта:" )}\n")
-        file.write(f"Email: {input('Введите email контакта: ')}\n")
-        file.write(f"Номер: {input('Введите номер контакта: ')}\n")
-        file.write("----------------------------------------------\n")
-
-def read_contacts():
-    with open('Contacts.txt', 'r', encoding='utf8') as file:
-        return file.read()
-    
-add_contact()
->>>>>>> a4772715d9c8f3291065ab48cbbbadb055430e40
 print(read_contacts())
--- lesson11.py
+++ lesson11.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import csv
 # # r, w, a, x
 
@@ -73,80 +72,4 @@ with open('data.csv', mode='w', newline='') as f:
                     
 #     return count, name_of_columns, round((value/counter), 3)
 
-=======
-import csv
-# # r, w, a, x
-
-
-# with open('data.csv', mode='r') as f:
-#     reader = csv.reader(f) # Созает список из списков([ [], [], [] ])
-#     for row in reader:
-#         print(row)
-
-# with open('data.csv', mode='r') as f:
-#     reader = csv.DictReader(f) # Создает список из словарей([{}, {}, {}])
-#     for row in reader:
-#         print(row['name'], row['age'])
-
-data = [['sasha', 'sash@gmail.com']]
-row = [{'name':'Sasha', 'age':15}, {'name':'Zhora', 'age':16} ]
-keys = ['name', 'age']
-with open('data.csv', mode='w', newline='') as f:
-    writer = csv.DictWriter(f, fieldnames=keys, delimiter='|')
-    writer.writeheader()
-    writer.writerows(row)
-
-# csv.register_dialect("semicolon",delimiter=';')
-# with open('data.csv', 'r') as f:
-#     reader =csv.reader(f, dialect='semicolon')
-#     for r in reader:
-#         print(r)
-# try:
-#     with open('employees.csv', 'r') as file_input, \
-#         open('data.txt', 'w', encoding='utf8') as file_output, \
-#         open('high_salary.csv', 'w', newline='') as f:
-#             reader = csv.DictReader(file_input)
-#             keys = ['name', 'salary']
-#             for employee in reader:
-#                 file_output.write(f'{employee['name']} работает в отделе {employee['department']} и получает заплату {employee['salary']}$\n')
-#                 if int(employee['salary']) > 60000:                           
-#                     writer = csv.DictWriter(f, fieldnames=keys)
-#                     writer.writeheader()
-#                     #    writer.writerow({'name': employee['name'], 'salary': employee['salary'] })
-                    
-#                     #    writer.writerow(employee)
-#                     print(employee)
-                                  
-# except FileExistsError:
-#        print('Данного файла не существует!')
-# except csv.Error as e:
-#        print(f'Библиотека csv выдает ошибку {e}')
-
-
-# def format(csv_file):
-#     # with open(csv_file, 'r') as f:
-#     f = open(csv_file, 'r')
-#     count = 0
-#     counter = 0
-#     summ = 0
-#     reader = csv.reader(f)
-#     for row in reader:
-#         if count == 0:
-#             name_of_columns = row
-#             name_of_columns = list(name_of_columns)
-#         count += 1
-#         for value in row:
-#             try:
-#                 value = int(value)
-#             except ValueError:
-#                 pass
-#             if isinstance(value, int):
-#                 summ += value
-#                 counter += 1
-
-
-                    
-#     return count, name_of_columns, round((value/counter), 3)
-
->>>>>>> a4772715d9c8f3291065ab48cbbbadb055430e40
 # print(format('employees.csv'))
